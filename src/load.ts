e22fdec7d00ab711d8d65ba0688e79898eb51f04
Romuald Brillout <git@brillout.com>
2022-04-07 10:51:28 +0200

    improve assertions

--- src/load.ts
+++ src/load.ts
@@ -1,14 +1,14 @@
 export { load }
 
 function load(assertFunctions: {
-  assert: (condition: unknown) => asserts condition
+  assert: (condition: unknown, debugInfo?: unknown) => asserts condition
   assertUsage: (condition: unknown, msg: string) => asserts condition
 }) {
   const moduleExports = require('./autoImporter')
   const keys = Object.keys(moduleExports)
   if (keys.length !== 0) {
-    assert(keys.length === 1)
-    assert(moduleExports.importerStatus === 'unset')
+    assert(keys.length === 1, { moduleExports })
+    assert(moduleExports.importerStatus === 'unset', { moduleExports })
     assertUsage(false, 'You seem to be using Yarn PnP.')
   }
 
@@ -16,8 +16,8 @@ function load(assertFunctions: {
 
   // Circumvent TS bug
   // https://github.com/microsoft/TypeScript/issues/36931
-  function assert(condition: unknown): asserts condition {
-    assertFunctions.assert(condition)
+  function assert(condition: unknown, debugInfo?: unknown): asserts condition {
+    assertFunctions.assert(condition, debugInfo)
   }
   function assertUsage(condition: unknown, msg: string): asserts condition {
     assertFunctions.assertUsage(condition, msg)
