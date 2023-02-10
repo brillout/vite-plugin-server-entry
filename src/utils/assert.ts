b8964f2f0d1c7bd6d7b5041fc1a30e7f4eb7a0e0
Romuald Brillout <git@brillout.com>
2023-02-10 12:51:49 +0100

    fix: improve assertion failure log

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -1,19 +1,37 @@
-const name = '@brillout/vite-plugin-import-build'
-const repo = 'https://github.com/brillout/vite-plugin-import-build/issues/new'
+export { assert }
 
-export function assert(condition: unknown, debugInfo?: unknown): asserts condition {
-  if (condition) return
+import { createErrorWithCleanStackTrace } from './createErrorWithCleanStackTrace'
+import { projectInfo } from './projectInfo'
 
-  const debugStr = debugInfo && (typeof debugInfo === 'string' ? debugInfo : '`' + JSON.stringify(debugInfo) + '`')
+const errorPrefix = `[${projectInfo.npmPackageName}@${projectInfo.projectVersion}]`
+const internalErrorPrefix = `${errorPrefix}[Bug]`
 
-  throw new Error(
+const numberOfStackTraceLinesToRemove = 2
+
+function assert(condition: unknown, debugInfo?: unknown): asserts condition {
+  if (condition) {
+    return
+  }
+
+  const debugStr = (() => {
+    if (!debugInfo) {
+      return null
+    }
+    const debugInfoSerialized = typeof debugInfo === 'string' ? debugInfo : '`' + JSON.stringify(debugInfo) + '`'
+    return `Debug info (this is for the ${projectInfo.projectName} maintainers; you can ignore this): ${debugInfoSerialized}`
+  })()
+
+  const internalError = createErrorWithCleanStackTrace(
     [
-      `[${name}][Bug] You stumbled upon a bug in the source code of ${name}.`,
-      `Reach out at ${repo} and include this error stack`,
-      '(the error stack is usually enough to fix the problem).',
-      debugStr && `(Debug info for the maintainers: ${debugStr})`
+      `${internalErrorPrefix} You stumbled upon a bug in ${projectInfo.projectName}'s source code.`,
+      `Reach out at ${projectInfo.githubRepository}/issues/new and include this error stack (the error stack is usually enough to fix the problem).`,
+      'A maintainer will fix the bug (usually under 24 hours).',
+      `Don't hesitate to reach out as it makes ${projectInfo.projectName} more robust.`,
+      debugStr
     ]
       .filter(Boolean)
-      .join(' ')
+      .join(' '),
+    numberOfStackTraceLinesToRemove
   )
+  throw internalError
 }
---
+++ src/utils/projectInfo.ts
@@ -0,0 +1,8 @@
+const PROJECT_VERSION = '0.1.11'
+
+export const projectInfo = {
+  projectName: '@brillout/vite-plugin-import-build' as const,
+  npmPackageName: '@brillout/vite-plugin-import-build' as const,
+  projectVersion: PROJECT_VERSION,
+  githubRepository: 'https://github.com/brillout/vite-plugin-import-build' as const
+}
