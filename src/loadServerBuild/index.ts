e99a4e600bc4872101daf0aea89648d9e8f8f2a7
Romuald Brillout <git@brillout.com>
2023-12-12 15:15:10 +0100

    fix: explicitly fail upon wrong disableAutoImporter usage

--- src/loadServerBuild/index.ts
+++ src/loadServerBuild/index.ts
@@ -11,10 +11,10 @@ async function loadServerBuild(outDir?: string): Promise<void | undefined> {
 
   debugLogsRuntimePre(autoImporter)
 
-  /* Is this better?
-  assertUsage(autoImporter.status !== 'DISABLED', "Make sure to not call loadServerBuild() if you disable the auto importer")
-  */
-  if (autoImporter.status === 'DISABLED') return
+  assertUsage(
+    autoImporter.status !== 'DISABLED',
+    "As a library author, if you disable the auto importer, then make sure your library injects a manual import of the file dist/server/importBuild.cjs into the built server entry file dist/server/index.js (or wherever the built server entry file is located) and make sure your library doesn't call loadServerBuild(), see https://github.com/brillout/vite-plugin-import-build#manual-import and https://github.com/brillout/vite-plugin-import-build#what-it-does"
+  )
 
   let success = false
   let requireError: unknown
