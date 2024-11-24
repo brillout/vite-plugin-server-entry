127945ae15b142a2f33b52294d39250cf3a81004
Romuald Brillout <git@brillout.com>
2024-11-24 20:37:24 +0100

    minor refactor

--- src/utils/getCwd.ts
+++ src/utils/getCwd.ts
@@ -1,6 +1,8 @@
+export { getCwd }
+
 import { toPosixPath } from './filesystemPathHandling'
 
-export function getCwd(): string | null {
+function getCwd(): string | null {
   if (typeof process == 'undefined' || !('cwd' in process)) return null
   return toPosixPath(process.cwd())
 }
