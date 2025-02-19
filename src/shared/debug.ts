05ff116dbd08dd251b92d3ae21e87fa727efb541
Romuald Brillout <git@brillout.com>
2025-02-19 23:19:31 +0100

    debug

--- src/shared/debug.ts
+++ src/shared/debug.ts
@@ -3,7 +3,7 @@ export { logDebug }
 
 import { logLabel } from './utils.js'
 
-const DEBUG: boolean = false
+const DEBUG: boolean = true
 
 function logDebug(...msgs: unknown[]) {
   console.log(`${logLabel}[DEBUG]`, ...msgs)
