02b590c169a8db7028b1ee7bd4e014a4fd478a62
Romuald Brillout <git@brillout.com>
2025-05-07 10:17:24 +0200

    fix: add DEBUG flag (#21)

--- src/shared/debug.ts
+++ src/shared/debug.ts
@@ -3,8 +3,21 @@ export { logDebug }
 
 import { logLabel } from './utils.js'
 
-const isDebug: boolean = false
+// We purposely read process.env.DEBUG early, in order to avoid users from the temptation to set process.env.DEBUG with JavaScript, since reading & writing process.env.DEBUG dynamically leads to inconsistencies such as https://github.com/vikejs/vike/issues/2239
+const DEBUG = getDEBUG() ?? ''
+const isDebug = DEBUG.includes('vite-plugin-server-entry')
+if (isDebug) Error.stackTraceLimit = Infinity
 
 function logDebug(...msgs: unknown[]) {
   console.log(`${logLabel}[DEBUG]`, ...msgs)
+}
+
+function getDEBUG() {
+  let DEBUG: undefined | string
+  // - `process` can be undefined in edge workers
+  // - We want bundlers to be able to statically replace `process.env.*`
+  try {
+    DEBUG = process.env.DEBUG
+  } catch {}
+  return DEBUG
 }
