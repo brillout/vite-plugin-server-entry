2c6d3fb5ccb88cec91b66b19633749449837dc05
Rom <git@brillout.com>
2025-08-25 18:00:48 +0200

    fix: fix server/client env check upon Vite 6 builder API (#23)

--- src/utils/isViteServerSide.ts
+++ src/utils/isViteServerSide.ts
@@ -1,3 +1,17 @@
-export function isViteServerSide(config: { build?: { ssr?: boolean | string } }): boolean {
-  return !!config?.build?.ssr
+export { isViteServerSide }
+
+import type { Environment } from 'vite'
+import { assert } from './assert.js'
+
+function isViteServerSide(
+  config: { build?: { ssr?: boolean | string } },
+  viteEnv:
+    | Environment
+    // Vite 5
+    | undefined,
+): boolean {
+  if (!viteEnv) return !!config?.build?.ssr
+  const { consumer } = viteEnv.config
+  assert(consumer === 'server' || consumer === 'client')
+  return consumer === 'server'
 }
